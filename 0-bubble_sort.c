536ad4c73a9a1c84ef5c73258bc78729e4f62501
Habeeb <tijanihabeeb12@outlook.com>
2023-04-29 21:31:53 +0100

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,27 +12,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j;
-  int temp;
-  int swaps;
+size_t i, j;
+int temp;
+int swaps;
 
-  for (i = 0; i < size; i++)
-  {
-    swaps = 0;
-    for (j = 0; j < size - i - 1; j++)
-    {
-      if (array[j] > array[j + 1])
-      {
-        temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
-        swaps = 1;
-        print_array(array, size);
-      }
-    }
-    if (swaps == 0)
-    {
-      break;
-    }
-  }
+for (i = 0; i < size; i++)
+{
+swaps = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swaps = 1;
+print_array(array, size);
+}
+}
+if (swaps == 0)
+{
+break;
+}
+}
 }
