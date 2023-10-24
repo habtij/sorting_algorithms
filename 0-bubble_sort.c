b2d40ff7b9eaaaf14761ddc0ab67c8b9a3dd2a06
Habeeb <tijanihabeeb12@outlook.com>
2023-10-24 17:04:56 +0100

    Add: 0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * bubble_sort - repeatedly steps through a list element by element,
+ * comparing the current element with the one after it, swapping their
+ * values if needed.
+ * @array: list item to be sorted
+ * @size: size of the array to be sorted
+ *
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+size_t i, j;
+int temp, swaps;
+
+for (i = 0; i < size; i++)
+{
+swaps = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j - 1])
+{
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+swaps = 1;
+print_array(array, size);
+}
+}
+if (swaps == 0)
+{
+break;
+}
+}
+}
